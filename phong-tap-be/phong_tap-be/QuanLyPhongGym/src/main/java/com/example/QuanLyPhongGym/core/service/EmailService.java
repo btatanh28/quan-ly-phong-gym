9f80d8452e253a4e5f4ef6281f1d52a9203eeb52
btatanh28 <upskh2801@gmail.com>
2026-03-10 10:49:04 +0700

    update email

--- phong-tap-be/phong_tap-be/QuanLyPhongGym/src/main/java/com/example/QuanLyPhongGym/core/service/EmailService.java
+++ phong-tap-be/phong_tap-be/QuanLyPhongGym/src/main/java/com/example/QuanLyPhongGym/core/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.example.QuanLyPhongGym.core.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -8,12 +9,16 @@ import org.springframework.stereotype.Service;
 public class EmailService {
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     public void sendVerificationEmail(String to, String code) {
         SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
         message.setTo(to);
         message.setSubject("Mã xác nhận đăng ký");
         message.setText("Mã xác nhận của bạn là: " + code);
