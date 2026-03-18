9b51f81f77babf47592b1cbe678ba96510849fbb
btatanh28 <upskh2801@gmail.com>
2026-03-19 00:04:31 +0700

    update mail

--- phong-tap-be/phong_tap-be/QuanLyPhongGym/src/main/java/com/example/QuanLyPhongGym/QuanLyPhongGymApplication.java
+++ phong-tap-be/phong_tap-be/QuanLyPhongGym/src/main/java/com/example/QuanLyPhongGym/QuanLyPhongGymApplication.java
@@ -2,8 +2,11 @@ package com.example.QuanLyPhongGym;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
+
 public class QuanLyPhongGymApplication {
 
 	public static void main(String[] args) {
--- phong-tap-be/phong_tap-be/QuanLyPhongGym/src/main/java/com/example/QuanLyPhongGym/core/service/EmailService.java
+++ phong-tap-be/phong_tap-be/QuanLyPhongGym/src/main/java/com/example/QuanLyPhongGym/core/service/EmailService.java
@@ -5,18 +5,23 @@ import com.sendgrid.helpers.mail.Mail;
 import com.sendgrid.helpers.mail.objects.Content;
 import com.sendgrid.helpers.mail.objects.Email;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
-    @Value("${sendgrid.api.key}")
-    private String apiKey;
+    @Autowired
+    private SendGrid sendGrid;
 
     @Value("${email.from}")
     private String fromEmail;
 
+    @Async
     public void sendVerificationEmail(String to, String code) {
+
+        long start = System.currentTimeMillis();
 
         Email from = new Email(fromEmail);
         Email toEmail = new Email(to);
@@ -24,24 +29,23 @@ public class EmailService {
         String subject = "Mã xác nhận đăng ký";
 
         Content content = new Content(
-                "text/plain",
-                "Mã xác nhận của bạn là: " + code);
+                "text/html",
+                "<h3>Mã xác nhận của bạn:</h3>" +
+                        "<h1 style='color:blue'>" + code + "</h1>");
 
         Mail mail = new Mail(from, subject, toEmail, content);
-
-        SendGrid sg = new SendGrid(apiKey);
 
         Request request = new Request();
 
         try {
-
             request.setMethod(Method.POST);
             request.setEndpoint("mail/send");
             request.setBody(mail.build());
 
-            Response response = sg.api(request);
+            Response response = sendGrid.api(request);
 
             System.out.println("Status code: " + response.getStatusCode());
+            System.out.println("SendGrid time: " + (System.currentTimeMillis() - start) + "ms");
 
         } catch (Exception e) {
             e.printStackTrace();
